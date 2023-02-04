19b6077a48ebf724e99e263c0a832da065c8ed65
vignesh7527 <124119637+vignesh7527@users.noreply.github.com>
2023-02-03 20:52:18 -0800

    Create n18.c

---
+++ n18.c
@@ -0,0 +1,21 @@
+//number of vowels given
+#include<stdio.h>
+#include<string.h>
+#define MAX 100
+main()
+{
+	char str[MAX];
+	int a,vowel,consonant,i;
+	printf("enter a string:");
+	scanf("%[^\n]s",&str);
+	vowel=0;
+	consonant=0;
+	a=strlen(str);
+	for(i=0;i<a;i++)
+	{
+		if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||
+		str[i]=='A'||str[i]=='E'||str[i]=='T'||str[i]=='O'||str[i]=='U')
+		vowel++;
+	}
+	printf("total number of vowel=%d\n",vowel);
+}
