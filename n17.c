d4a1259ec4276b8cb193cc363de5927590c58232
vignesh7527 <124119637+vignesh7527@users.noreply.github.com>
2023-02-03 20:51:05 -0800

    Create n17.c

---
+++ n17.c
@@ -0,0 +1,21 @@
+//sum of elements
+#include<stdio.h>
+main()
+{
+	int a[25],n,i;
+	float avg=0,sum=0;
+	printf("enter the number of element in array:");
+	scanf("%d",&n);
+	printf("enter an element:\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+	sum=sum+a[i];
+	avg=sum/n;
+	}
+	printf("\n sum of element of array is :%.2f",sum);
+	printf("\n average of element of array are :%2.f",avg);
+}
