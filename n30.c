4ea032c5cbffbdb3e6834266828febaffb527ced
vignesh7527 <124119637+vignesh7527@users.noreply.github.com>
2023-02-03 22:53:30 -0800

    Create n30.c

---
+++ n30.c
@@ -0,0 +1,17 @@
+//valid or invalid//
+#include <stdio.h>
+#include <stdlib.h>
+int main(void) 
+{
+int menu;
+printf("Enter a Menu Option: ");
+scanf("%d", &menu);
+if( menu != 1)
+{
+printf("Invalid Entry!");
+} else 
+{
+printf("Your Menu Option is: %d\n", menu);
+}
+return 0;
+}
