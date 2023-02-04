7575d02bddae287c9df277257ebcb13762ce6007
vignesh7527 <124119637+vignesh7527@users.noreply.github.com>
2023-02-04 12:45:03 +0530

    Create n32.c

---
+++ n32.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("ENTER A NUMBER");
+scanf("%d",&n);
+printf("\nsquare of a number %d is %d",n,n*n);
+printf("\ncube of a number %d is %d",n,n*n*n);
+return 0;
+}
