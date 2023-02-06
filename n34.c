c79ff13427402769473fbafadea2ae9ca0ee648c
vignesh7527 <124119637+vignesh7527@users.noreply.github.com>
2023-02-05 20:27:11 -0800

    Create n34.c

---
+++ n34.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("ENTER SUBJECT 1 MARKS : ");
+scanf("%d",&a);
+int b;
+printf("ENTER SUBJECT 2 MARKS : ");
+scanf("%d",&b);
+int c;
+printf("ENTER SUBJECT 3 MARKS : ");
+scanf("%d",&c);
+int d;
+printf("ENTER SUBJECT 4 MARKS : ");
+scanf("%d",&d);
+int e;
+e=a+b+c+d;
+int marks;
+marks=e/4;
+printf("TOTAL PERCENTAGE : %d", marks);
+printf("                                                      ");
+printf("                                                      ");
+{
+if(marks<40)
+printf("FAIL");
+else if(marks>=40 && marks<50)
+printf("THIRD CLASS");
+else if(marks>=50 && marks<60)
+printf("SECOND CLASS");
+else if(marks>=60 && marks<75)
+printf("FIRST CLASS");
+else if(marks>=75 && marks<100)
+printf("DISTINATION");
+}
+   return 0;
+}
